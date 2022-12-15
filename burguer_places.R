6b1c2c95b20ef85846a3c97c6e01c4a264834eee
Kalincillo <82189871+Kalincillo@users.noreply.github.com>
2022-12-15 14:14:26 -0600

    fix api key .Renviron

--- burguer_places.R
+++ burguer_places.R
@@ -3,21 +3,20 @@ library(mapproj)
 library(googleway)
 
 
-key <- Sys.getenv("pw")
+pass <- Sys.getenv("PASSWORD")
 register_google(key)
 
 places <- google_places(search_string = 'ramen', 
                         location=c(20.687408505134194, -103.35158714983811), 
-                        radius=5000, key=llave)
+                        radius=5000, key=pass)
 
 places$results$geometry$location
 
 places_2 <- google_places(search_string = 'ramen', 
                         location=c(20.687408505134194, -103.35158714983811), 
-                        radius=5000, key=llave, 
+                        radius=5000, key=pass, 
                         page_token=places$next_page_token )
 places_2
 
 qmap('Guadalajara', zoom = 12, maptype = "satellite")
 
-getwd()
