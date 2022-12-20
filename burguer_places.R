08e4f5c1d45a9851bb2e1dae669e27123bda4d66
Kalincillo <oscar.delacruz19f@gmail.com>
2022-12-19 21:30:51 -0600

    add coordinates of first 20 places

--- burguer_places.R
+++ burguer_places.R
@@ -1,10 +1,11 @@
 library(ggmap)
 library(mapproj)
 library(googleway)
+library(sp)
 
 
 pass <- Sys.getenv("PASSWORD")
-register_google(key)
+register_google(pass)
 
 places <- google_places(search_string = 'ramen', 
                         location=c(20.687408505134194, -103.35158714983811), 
@@ -12,7 +13,7 @@ places <- google_places(search_string = 'ramen',
 
 places$results$geometry$location
 
-places_2 <- google_places(search_string = 'ramen', 
+places_2 <- google_places(search_string = 'burguer', 
                         location=c(20.687408505134194, -103.35158714983811), 
                         radius=5000, key=pass, 
                         page_token=places$next_page_token )
@@ -20,3 +21,17 @@ places_2
 
 qmap('Guadalajara', zoom = 12, maptype = "satellite")
 
+qmap('Eutimio Pinzón 927', zoom=19, maptype = "satellite")
+
+# coord_1 <- 
+lat_1 <- places$results$geometry$viewport
+lat_1
+
+lat_2 <- places$results$geometry$location
+lat_2
+
+# Convert burguer places into Spacial points
+coords <- cbind(Longitude=as.numeric(as.character(lat_2$lng)),
+                Latitude = as.numeric(as.character(lat_2$lat)))
+
+burguer_points <- SpatialPointsDataFrame(coords)
