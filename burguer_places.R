f05f02ae5f70e659c44494052da5452387f0a44c
Kalincillo <82189871+Kalincillo@users.noreply.github.com>
2022-12-28 13:31:57 -0600

    reformat code

--- burguer_places.R
+++ burguer_places.R
@@ -1,5 +1,6 @@
-# This code will find the best reviewed burger places in my local town
-# ZMG and the distance between them, i will use the rating system from
+# This code will find the best reviewed burger places
+# in ZMG (Guadalajara, Zapopan, Tlaquepaque, Tonala, Tlajomulco, El Salto)
+# and the distance between them, i will use the rating system from
 # Google maps and connect to the Google cloud API with R
 
 library(ggmap)
@@ -11,25 +12,19 @@ library(dplyr)
 # register to the API
 register_google(Sys.getenv("PASSWORD"))
 
-places <- google_places(search_string = 'ramen', 
-                        location=c(20.687408505134194, -103.35158714983811), 
-                        radius=5000, key=Sys.getenv("PASSWORD"))
+# search for places including burger from the center of the city with a
+# 10km radius
+places <- google_places(search_string = 'burger', 
+                        location=c(20.677214679298075, -103.34695181349188), 
+                        radius=10000, key=Sys.getenv("PASSWORD"))
 
-places$results$geometry$location
+# get latitude and longitude of the first 20 places
+lat_lon <- places$results$geometry$location
 
+# Convert burger places into Spacial points
+coords <- cbind(Longitude=as.numeric(as.character(lat_lon$lng)),
+                Latitude = as.numeric(as.character(lat_lon$lat)))
 
-# coord_1 <- aa
-lat_1 <- places$results$geometry$viewport
-lat_1
-
-lat_2 <- places$results$geometry$location
-lat_2
-
-# Convert burguer places into Spacial points
-coords <- cbind(Longitude=as.numeric(as.character(lat_2$lng)),
-                Latitude = as.numeric(as.character(lat_2$lat)))
-
-coords
 places_df <- data.frame(places$results)
 
 burguer_points <- 
@@ -37,11 +32,11 @@ burguer_points <-
                          places_df[-3],
                          proj4string = CRS("+init=epsg:4326"))
 
-plot(burguer_points, pch=".", col="darkred")
+plot(burguer_points, pch=".", col="darkblue")
 
 burguer_map <- qmap("guadalajara", zoom=13, maptype="hybrid")
 
-burguer_map + geom_point(data = places_df, 
+burger_gmap <- burguer_map + geom_point(data = places_df, 
                          aes(x = places_df$geometry$location$lng, 
                              y = places_df$geometry$location$lat),
                          color="red", size=3, alpha=0.5)
