2f17ad7856bf68c4ece493a4c407bc53cea78f87
Kalincillo <oscar.delacruz19f@gmail.com>
2023-01-03 19:56:38 -0600

    format code and add next tokens for the next searches

--- burguer_places.R
+++ burguer_places.R
@@ -17,8 +17,9 @@ ton <- c(20.623958671818926, -103.24131114715028)
 tlj <- c(20.475015349386634, -103.44819685025686)
 sal <- c(20.519990183452695, -103.17931472569043)
 
-# radius
-rad <- 5000
+# radius of approximately 3 km, if it is longer it can go beyond the 
+# limit of the zone
+rad <- 3000
 
 # register to the API
 register_google(Sys.getenv("PASSWORD"))
@@ -27,39 +28,117 @@ register_google(Sys.getenv("PASSWORD"))
 # 10km radius
 
 # ---------------------------------------------------------------------------
-# Guadalajara burger places
+# Guadalajara burger places 1, 2 and 3 token
 gdl_burger <- google_places(search_string='burger',
                             location=gdl,
                             radius=rad,
                             key=Sys.getenv("PASSWORD"))
+
+# gdl_burger_2 <- google_places(search_string='burger',
+#                             location=gdl,
+#                             radius=rad,
+#                             key=Sys.getenv("PASSWORD"),
+#                             page_token=gdl_burger$next_page_token)
+# 
+# gdl_burger_3 <- google_places(search_string='burger',
+#                               location=gdl,
+#                               radius=rad,
+#                               key=Sys.getenv("PASSWORD"),
+#                               page_token=gdl_burger_2$next_page_token)
+
 # Zapopan burger places
 zap_burger <- google_places(search_string='burger',
                             location=zap,
                             radius=rad,
                             key=Sys.getenv("PASSWORD"))
+
+# zap_burger_2 <- google_places(search_string='burger',
+#                             location=zap,
+#                             radius=rad,
+#                             key=Sys.getenv("PASSWORD"),
+#                             page_token=zap_burger$next_page_token)
+# 
+# zap_burger_3 <- google_places(search_string='burger',
+#                             location=zap,
+#                             radius=rad,
+#                             key=Sys.getenv("PASSWORD"),
+#                             page_token=zap_burger_2$next_page_token)
+
 # Tlaquepaque burger places
 tlq_burger <- google_places(search_string='burger',
                             location=tlq,
                             radius=rad,
                             key=Sys.getenv("PASSWORD"))
+
+# tlq_burger_2 <- google_places(search_string='burger',
+#                             location=tlq,
+#                             radius=rad,
+#                             key=Sys.getenv("PASSWORD"),
+#                             page_token=tlq_burger$next_page_token)
+# 
+# tlq_burger_3 <- google_places(search_string='burger',
+#                               location=tlq,
+#                               radius=rad,
+#                               key=Sys.getenv("PASSWORD"),
+#                               page_token=tlq_burger_2$next_page_token)
 # Tonala burger places
 ton_burger <- google_places(search_string='burger',
                             location=ton,
                             radius=rad,
                             key=Sys.getenv("PASSWORD"))
+
+# ton_burger_2 <- google_places(search_string='burger',
+#                             location=ton,
+#                             radius=rad,
+#                             key=Sys.getenv("PASSWORD"),
+#                             page_token=ton_burger$next_page_token)
+# 
+# ton_burger_3 <- google_places(search_string='burger',
+#                               location=ton,
+#                               radius=rad,
+#                               key=Sys.getenv("PASSWORD"),
+#                               page_token=ton_burger_2$next_page_token)
+
 # Tlajomulco burger places
 tlj_burger <- google_places(search_string='burger',
                             location=tlj,
                             radius=rad,
                             key=Sys.getenv("PASSWORD"))
+
+# tlj_burger_2 <- google_places(search_string='burger',
+#                             location=tlj,
+#                             radius=rad,
+#                             key=Sys.getenv("PASSWORD"),
+#                             page_token=tlj_burger$next_page_token)
+# 
+# tlj_burger_3 <- google_places(search_string='burger',
+#                               location=tlj,
+#                               radius=rad,
+#                               key=Sys.getenv("PASSWORD"),
+#                               page_token=tlj_burger_2$next_page_token)
 # El salto burger places
-sal <- google_places(search_string='burger',
+sal_burger <- google_places(search_string='burger',
                      location=sal,
                      radius=rad,
                      key=Sys.getenv("PASSWORD"))
-# ---------------------------------------------------------------------------
 
-# get latitude and longitude of the first 20 places
+# sal_burger_2 <- google_places(search_string='burger',
+#                      location=sal,
+#                      radius=rad,
+#                      key=Sys.getenv("PASSWORD"),
+#                      page_token=sal$next_page_token)
+# 
+# sal_burger_3 <- google_places(search_string='burger',
+#                        location=sal,
+#                        radius=rad,
+#                        key=Sys.getenv("PASSWORD"),
+#                        page_token=sal_2$next_page_token)
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# get latitude and longitude of the first 20 places for each search
+
+# first 20 searches
 gdl_coords <- gdl_burger$results$geometry$location
 zap_coords <- zap_burger$results$geometry$location
 tlq_coords <- tlq_burger$results$geometry$location
@@ -67,8 +146,57 @@ ton_coords <- ton_burger$results$geometry$location
 tlj_coords <- tlj_burger$results$geometry$location
 sal_coords <- sal_burger$results$geometry$location
 
+# # second 20 searches
+# gdl_coords_2 <- gdl_burger_2$results$geometry$location
+# zap_coords_2 <- zap_burger_2$results$geometry$location
+# tlq_coords_2 <- tlq_burger_2$results$geometry$location
+# ton_coords_2 <- ton_burger_2$results$geometry$location
+# tlj_coords_2 <- tlj_burger_2$results$geometry$location
+# sal_coords_2 <- sal_burger_2$results$geometry$location
+# 
+# # third 20 searches
+# gdl_coords_3 <- gdl_burger_3$results$geometry$location
+# zap_coords_3 <- zap_burger_3$results$geometry$location
+# tlq_coords_3 <- tlq_burger_3$results$geometry$location
+# ton_coords_3 <- ton_burger_3$results$geometry$location
+# tlj_coords_3 <- tlj_burger_3$results$geometry$location
+# sal_coords_3 <- sal_burger_3$results$geometry$location
 
-# Convert burger places into Spacial points
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# get name of places of the first 20 places for each search
+
+# first 20 searches
+gdl_places <- gdl_burger$results$name
+zap_places <- zap_burger$results$name
+tlq_places <- tlq_burger$results$name
+ton_places <- ton_burger$results$name
+tlj__places <- tlj_burger$results$name
+sal_places <- sal_burger$results$name
+
+# # second 20 searches
+# gdl_places_2 <- gdl_burger_2$results$name
+# zap_places_2 <- zap_burger_2$results$name
+# tlq_places_2 <- tlq_burger_2$results$name
+# ton_places_2 <- ton_burger_2$results$name
+# tlj__places_2 <- tlj_burger_2$results$name
+# sal_places_2 <- sal_burger_2$results$name
+# 
+# # third 20 searches
+# gdl_places_3 <- gdl_burger_3$results$name
+# zap_places_3 <- zap_burger_3$results$name
+# tlq_places_3 <- tlq_burger_3$results$name
+# ton_places_3 <- ton_burger_3$results$name
+# tlj__places_3 <- tlj_burger_3$results$name
+# sal_places_3 <- sal_burger_3$results$name
+
+# -----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
+# *************** Convert burger places into Spacial points *******************
+
+# first 20 searches
 sp_gdl <- cbind(Longitude=as.numeric(as.character(gdl_coords$lng)),
                 Latitude = as.numeric(as.character(gdl_coords$lat)))
 
@@ -87,8 +215,44 @@ sp_tlj <- cbind(Longitude=as.numeric(as.character(tlj_coords$lng)),
 sp_sal <- cbind(Longitude=as.numeric(as.character(sal_coords$lng)),
                 Latitude = as.numeric(as.character(sal_coords$lat)))
 
-coords <- cbind(Longitude=as.numeric(as.character(lat_lon$lng)),
-                Latitude = as.numeric(as.character(lat_lon$lat)))
+# # second 20 searches
+# sp_gdl_2 <- cbind(Longitude=as.numeric(as.character(gdl_coords_2$lng)),
+#                 Latitude = as.numeric(as.character(gdl_coords_2$lat)))
+# 
+# sp_zap_2 <- cbind(Longitude=as.numeric(as.character(zap_coords_2$lng)),
+#                 Latitude = as.numeric(as.character(zap_coords_2$lat)))
+# 
+# sp_tlq_2 <- cbind(Longitude=as.numeric(as.character(tlq_coords_2$lng)),
+#                 Latitude = as.numeric(as.character(tlq_coords_2$lat)))
+# 
+# sp_to_2 <- cbind(Longitude=as.numeric(as.character(ton_coords_2$lng)),
+#                 Latitude = as.numeric(as.character(ton_coords_2$lat)))
+# 
+# sp_tlj_2 <- cbind(Longitude=as.numeric(as.character(tlj_coords_2$lng)),
+#                 Latitude = as.numeric(as.character(tlj_coords_2$lat)))
+# 
+# sp_sal_2 <- cbind(Longitude=as.numeric(as.character(sal_coords_2$lng)),
+#                 Latitude = as.numeric(as.character(sal_coords_2$lat)))
+# 
+# # third 20 searches
+# sp_gdl_3 <- cbind(Longitude=as.numeric(as.character(gdl_coords_3$lng)),
+#                   Latitude = as.numeric(as.character(gdl_coords_3$lat)))
+# 
+# sp_zap_3 <- cbind(Longitude=as.numeric(as.character(zap_coords_3$lng)),
+#                   Latitude = as.numeric(as.character(zap_coords_3$lat)))
+# 
+# sp_tlq_3 <- cbind(Longitude=as.numeric(as.character(tlq_coords_3$lng)),
+#                   Latitude = as.numeric(as.character(tlq_coords_3$lat)))
+# 
+# sp_to_3 <- cbind(Longitude=as.numeric(as.character(ton_coords_3$lng)),
+#                  Latitude = as.numeric(as.character(ton_coords_3$lat)))
+# 
+# sp_tlj_3 <- cbind(Longitude=as.numeric(as.character(tlj_coords_3$lng)),
+#                   Latitude = as.numeric(as.character(tlj_coords_3$lat)))
+# 
+# sp_sal_3 <- cbind(Longitude=as.numeric(as.character(sal_coords_2$lng)),
+#                   Latitude = as.numeric(as.character(sal_coords_2$lat)))
+# -----------------------------------------------------------------------------
 
 places_df <- data.frame(places$results)
 
